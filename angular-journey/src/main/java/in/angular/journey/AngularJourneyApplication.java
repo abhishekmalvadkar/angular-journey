0d17c0cc4c1cb5c241820f695705dd7ff788d933
Abhishek Malvadkar <abhishek@dev.com>
2023-07-09 18:55:45 +0530

    Created spring boot project

---
+++ angular-journey/src/main/java/in/angular/journey/AngularJourneyApplication.java
@@ -0,0 +1,13 @@
+package in.angular.journey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularJourneyApplication.class, args);
+	}
+
+}
